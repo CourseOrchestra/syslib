31fb5b4bc6df7ffe3f346af806c26f23e68cdbf0
m.prudyvus <prudyvus.mariya@physics.msu.ru>
2018-04-12 15:07:48 +0000

    logger
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@2201 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/__init__.py
+++ score/common/__init__.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import os
+import logging.config
 from common.navigator import navigatorsParts, seriesNavigator
 
 globalCelesta = None
@@ -19,3 +21,38 @@ if isInitContext:
         permInit(context)
 
 navigatorsParts['numberSeries'] = seriesNavigator
+
+# Запускаем логгер
+# Сначала ищем настройку в celesta.properties
+log_config_file = globalCelesta.getSetupProperties().getProperty('loggingconf.path')
+# Если настройки нет, ищем в корне юзердаты
+if not log_config_file:
+    try:
+        from ru.curs.showcase.runtime import AppInfoSingleton
+
+        log_config_file = os.path.join(AppInfoSingleton.getAppInfo().getUserdataRoot(), 'logging.ini')
+    except ImportError:
+        pass
+if log_config_file and os.path.exists(log_config_file):
+    # Если нашли настройку и файл существует
+    logging.config.fileConfig(log_config_file)
+    log = logging.getLogger("solution")
+
+    log.debug('logger file config - {}'.format(log_config_file))
+else:
+    # Иначе формат по-умолчанию
+    log_format = '%(asctime)s %(levelname)s [%(name)s] %(message)s'
+    logging.basicConfig(format=log_format, level=logging.NOTSET)
+
+    formatter = logging.Formatter(log_format)
+
+    log = logging.getLogger("solution")
+
+    streamHandler = logging.StreamHandler()
+    streamHandler.setLevel(logging.DEBUG)
+    streamHandler.setFormatter(formatter)
+
+    log.addHandler(streamHandler)
+    log.propagate = False  # Предотвращает дублирование логов
+
+    log.debug("logger basic config")
